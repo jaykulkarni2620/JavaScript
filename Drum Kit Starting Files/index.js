8824f6cab3c9018a2f20fa61107351ede912aa3f
jaykulkarni2620 <kulkarnijay76@gmail.com>
2023-11-21 18:13:05 +0530

    drum kit challange

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,36 @@
+var totalNumbers = document.querySelectorAll(".drum").length;
+
+for ( var i = 0 ; i<totalNumbers; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", clickable);
+
+function clickable() {
+
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var audio = new Audio ('sounds/snare.mp3');
+// audio.play();
