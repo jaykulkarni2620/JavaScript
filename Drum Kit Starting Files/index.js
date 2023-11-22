3856d205ffda17036549f44874a57e7b4b64c191
jaykulkarni2620 <kulkarnijay76@gmail.com>
2023-11-22 07:32:01 +0530

    swichCase

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,13 +6,45 @@ document.querySelectorAll(".drum")[i].addEventListener("click", clickable);
 
 function clickable() {
 
-    switch (key) {
-        case value:
-            
-            break;
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var snare = new Audio ('sounds/snare.mp3');
+            snare.play();
+        break;
+
+        case "a":
+            var kick = new Audio ('sounds/kick-bass.mp3');
+            kick.play();
+        break;
+
+        case "s":
+            var tom1 = new Audio ('sounds/tom-1.mp3');
+            tom1.play();
+        break;
+
+        case "d":
+            var tom2 = new Audio ('sounds/tom-2.mp3');
+            tom2.play();
+        break;
+
+        case "j":
+            var tom3 = new Audio ('sounds/tom-3.mp3');
+            tom3.play();
+        break;
+
+        case "k":
+            var tom4 = new Audio ('sounds/tom-4.mp3');
+            tom4.play();
+        break;
+
+        case "l":
+            var crash = new Audio ('sounds/crash.mp3');
+            crash.play();
+        break;
     
         default:
-            break;
     }
 
     }
@@ -32,5 +64,3 @@ function clickable() {
 
 
 
-// var audio = new Audio ('sounds/snare.mp3');
-// audio.play();
